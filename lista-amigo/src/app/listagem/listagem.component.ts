3d463db68dba3d3df1ff06d047a1cbf9c45a23db
Luan <santos.pereira.luan@gmail.com>
2017-11-29 08:25:15 -0200

    http inicio

--- lista-amigo/src/app/listagem/listagem.component.ts
+++ lista-amigo/src/app/listagem/listagem.component.ts
@@ -1,6 +1,6 @@
 import { Component, AfterViewInit} from '@angular/core';
 import { ItemAmigo } from './ItemAmigo';
-import * as $ from 'jquery';
+import { Http } from '@angular/http';
 
 @Component({
   selector: 'app-listagem',
@@ -11,17 +11,25 @@ export class ListagemComponent {
 
   lestOpen: ItemAmigo = null;
 
+  constructor(http: Http){
+    let stream = http.get('http://localhost:8080/lista-amigo/app/amigo/');
+    stream.subscribe(response => {
+      this.amigos = response.json();
+      console.log(this.amigos);
+    });
+  }
+
   amigos: ItemAmigo[] = [
-    {
-      amigo: {
-        id: 1, 
-        nome: 'Luan'
-      }, 
-      hidden: true
-    },
-    {amigo: {id: 2, nome: 'Ana'}, hidden: true},
-    {amigo: {id: 3, nome: 'Minon'}, hidden: true},
-    {amigo: {id: 4, nome: 'Rosa'}, hidden: true}
+    // {
+    //   amigo: {
+    //     id: 1, 
+    //     nome: 'Luan'
+    //   }, 
+    //   hidden: true
+    // },
+    // {amigo: {id: 2, nome: 'Ana'}, hidden: true},
+    // {amigo: {id: 3, nome: 'Minon'}, hidden: true},
+    // {amigo: {id: 4, nome: 'Rosa'}, hidden: true}
   ];
 
   toggle(item: ItemAmigo){
